43c1d9ecc20cae018a92f58550290d82d4436b97
Ekaterina Voytovich <e.voytovich@anoki.it>
2019-10-28 16:29:26 +0100

    feat: add  sum function

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function flatArray(arr) {
-  // your code here ...
-}
+  if(!arr || typeof arr==='string') return []
+    return arr.reduce(function (flat, toFlatten) {
+      return flat.concat(Array.isArray(toFlatten) ? flatArray(toFlatten) : toFlatten);
+    }, []);
+  };
+
 
 function sum(arr) {
-  // your code here...
+  if (!arr || typeof arr === "string") return 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== "number" && Array.isArray(arr[i]) === false)
+      return "error";
+  }
+  if (arr.length == 1) return arr[0];
+  return arr[0] + sum(arr.slice(1));
 }
 
-module.exports = flatArray;
+
+
+module.exports = {flatArray, sum}
+
